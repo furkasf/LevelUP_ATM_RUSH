e3d03e23b146343bc77279227ecb7b5ab1971a90
hakanmelen <melenglobal@gmail.com>
2022-07-12 17:38:45 +0300

    first blood

---
+++ Assets/Scripts/Managers/CollactableManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollactableManager : MonoBehaviour
+{   
+    private string greeting= "Hello World";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(greeting);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
