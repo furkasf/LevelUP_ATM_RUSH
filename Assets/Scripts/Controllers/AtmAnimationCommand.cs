1d23261129a0af988ed346769fcf559e6910823c
furkasf <furkantural3328@hotmail.com>
2022-07-24 03:40:14 +0300

    atm animation implamented after player touch the atm

---
+++ Assets/Scripts/Controllers/AtmAnimationCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers
+{
+    public class AtmAnimationCommand : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.CompareTag("Player"))
+            {
+                transform.DOMoveY(-10, 1f).SetEase(Ease.Linear);
+            }
+        }
+    }
+}
