50a5be7fcb105e56d4488bb96e7bfa49870ea4f7
furkasf <furkantural3328@hotmail.com>
2022-07-25 22:27:56 +0300

    obstical animations implamented

--- Assets/Scenes/Minigame.cs
+++ Assets/Scenes/Minigame.cs
@@ -10,18 +10,21 @@ public class Minigame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Mini(20, 4));
     }
 
-    public void Mini(int score, float delay)
+    public IEnumerator Mini(int score, float delay = 0.2f)
     {
         
         for(int i = 0; i < score; i++)
         {
             GameObject obj = MoneyPoolManager.instance.GetMoneyFromPool();
+            obj.SetActive(true);
             obj.transform.position = transform.position;
-            transform.position += Vector3.up;
-           
+            // DOVirtual.DelayedCall(5, () => transform.DOMoveY(1, 0.5f).SetRelative(obj.transform));
+            transform.DOMoveY(1, 0.1f).SetRelative(obj.transform);
+            yield return new WaitForSeconds(0.09f);
+            Debug.Log("index : " + i);
         }
         
     }
