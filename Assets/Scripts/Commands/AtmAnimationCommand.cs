4d52798d484b3131e8b4e17c435c528a52cf7079
furkasf <furkantural3328@hotmail.com>
2022-08-01 16:00:25 +0300

    now atms shaking when money put in it

--- Assets/Scripts/Commands/AtmAnimationCommand.cs
+++ Assets/Scripts/Commands/AtmAnimationCommand.cs
@@ -11,6 +11,10 @@ namespace Commands
             {
                 transform.DOMoveY(-10, 1f).SetEase(Ease.Linear);
             }
+            if(other.CompareTag("Collected"))
+            {
+                transform.DOShakePosition(0.02f, 0.4f, 2);
+            }
         }
     }
 }
